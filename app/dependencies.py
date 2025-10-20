0f3ec3a08ee4cd217c2e646fe31c643dd2c24be1
pollyana <pollyana.rocha416@gmail.com>
2025-10-20 19:10:07 -0400

    yield -> retorna a session, mas nao encerrar a execução da funcao. finally -> pra sessao com o banco de dados seja fechada independente do oque acontecer

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,6 +2,9 @@ from sqlalchemy.orm import sessionmaker
 from app.db.models import db
 
 def pegar_sessao():
-    Session = sessionmaker(bind=db)
-    session = Session()
-    return session
+    try:
+        Session = sessionmaker(bind=db)
+        session = Session()
+        yield session
+    finally:
+        session.close()
