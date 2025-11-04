e3f19d57ac7c5af0f1c3e70fd502b45a12a72d1e
pollyana <pollyana.rocha416@gmail.com>
2025-11-04 19:27:38 -0400

    adicionando logs pra create_order

--- app/routes/order_routes.py
+++ app/routes/order_routes.py
@@ -1,9 +1,15 @@
+import logging
+import traceback
 from fastapi import APIRouter, Depends
 from app.schemas.order_schemas import OrderSchema
 from app.dependencies import pegar_sessao
 from sqlalchemy.orm import Session
 from app.db.models import Pedido
+from app.logging_config import setup_logging
 
+
+setup_logging()
+logger = logging.getLogger("my_app")
 
 order_router = APIRouter(prefix="/orders", tags=["orders"])
 
@@ -35,8 +41,14 @@ async def orders():
     }
     )
 async def create_order(order_schema: OrderSchema, session: Session=Depends(pegar_sessao)):
-    new_order = Pedido(usuario=order_schema.id_usuario)
-    session.add(new_order)
-    session.commit()
-    
-    return {"message": f"Create order: {new_order.id}"}
+    try:
+        new_order = Pedido(usuario=order_schema.id_usuario)
+        session.add(new_order)
+        session.commit()
+        
+        logger.info(f"POST create_order {order_schema.id_usuario} | 201 Created")
+        return {"message": f"Create order: {new_order.id}"}
+    except Exception as e:
+        logger.error(f"POST criar_conta {order_schema.id_usuario} | 500 ERRO | {traceback.format_exception(type(e), e, e.__traceback__)}")
+        session.rollback()
+        raise e
