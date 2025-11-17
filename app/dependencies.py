bb60a7e4cf73b739b7af2efe176af588fadc4e8b
pollyana <pollyana.rocha416@gmail.com>
2025-11-17 19:08:08 -0400

    Movendo funcao vefify_jwt_token para o arquivo dependencies

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,8 @@
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from fastapi import Depends
 from app.db.models import db
+from app.db.models import Usuario
+
 
 def pegar_sessao():
     try:
@@ -8,3 +11,9 @@ def pegar_sessao():
         yield session
     finally:
         session.close()
+
+
+def verify_jwt_token(token: str, session: Session=Depends(pegar_sessao)):
+    user = session.query(Usuario).filter(Usuario.id==1).first()
+    
+    return user
