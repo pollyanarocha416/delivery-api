348039726c3455f5155a03ff6ee753c6e176bb34
pollyanarocha416 <pollyanarocha416@gmail.com>
2025-12-10 18:34:49 -0400

    refactor: organizando imports

--- app/routes/order_routes.py
+++ app/routes/order_routes.py
@@ -3,11 +3,11 @@ import traceback
 from jose import JWTError
 from fastapi import APIRouter, Depends, HTTPException
 from typing import List, Literal, Optional, cast
-from sqlalchemy.orm import Session
+from app.dependencies import pegar_sessao, verify_jwt_token
 from app.logging_config import setup_logging
 from app.schemas.order_schemas import OrderResponse, OrderSchema, ItemOrderSchema
 from app.db.models import Pedido, Usuario, ItensPedido
-from app.dependencies import pegar_sessao, verify_jwt_token
+from sqlalchemy.orm import Session
 
 
 setup_logging()
