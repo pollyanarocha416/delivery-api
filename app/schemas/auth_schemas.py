d3ba2d8c077ce201ecf5a3dbb1b86b6f8bbe78fa
pollyanarocha416 <pollyanarocha416@gmail.com>
2026-01-14 20:20:17 -0400

    FEAT: Melhorando schema dos usuarios, adicionando tipagem pro tamanho da senha

--- app/schemas/auth_schemas.py
+++ app/schemas/auth_schemas.py
@@ -5,7 +5,7 @@ from typing import Optional
 class UserSchema(BaseModel):
     nome: str = Field(..., min_length=1, max_length=45, description="Nome do usuário")
     email: str = Field(..., min_length=5, max_length=100, description="Email do usuário")
-    senha: str = Field(..., min_length=8, max_length=45, description="Senha do usuário")
+    senha: str = Field(..., min_length=3, max_length=45, description="Senha do usuário")
     ativo: Optional[bool] = Field(None, description="Indica se o usuário está ativo")
     admin: Optional[bool] = Field(None, description="Indica se o usuário tem privilégios de administrador")
     
@@ -15,7 +15,7 @@ class UserSchema(BaseModel):
 
 class LoginSchema(BaseModel):
     email: str = Field(..., min_length=5, max_length=100, description="Email do usuário")
-    senha: str = Field(..., min_length=1, description="Senha do usuário")
+    senha: str = Field(..., min_length=3, max_length=45, description="Senha do usuário")
     
     class Config:
         from_attributes = True
