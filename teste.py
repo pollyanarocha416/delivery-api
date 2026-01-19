6ab7f53859b740beb29751fc230ffc27925d37f1
pollyanarocha416 <pollyanarocha416@gmail.com>
2026-01-19 19:58:24 -0400

    Ha fazer

---
+++ teste.py
@@ -0,0 +1,8 @@
+# Ha fazer
+
+# 1. Adicionar funcao ao criar um nova pedido, colocar o que sera pedido, esses items devem esta em uma tabela ja definido
+# 2. Adicionar schema de retorno nos outros endpoints
+# 2. Adicionar limite de pedidos, baseado em um valor maximo, definido em uma tabela
+#     3. mesma coisa no itens
+# 4. Adicionar tela principal que explica a API, com os endpoints disponiveis e como usar cada um
+
