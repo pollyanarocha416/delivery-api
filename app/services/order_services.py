b24fa0a4a510218934bbff615d11e69a87b9a961
pollyanarocha416 <pollyanarocha416@gmail.com>
2026-02-23 19:57:11 -0400

    REFACTOR: removendo parametro que nao esta sendo usado

--- app/services/order_services.py
+++ app/services/order_services.py
@@ -3,8 +3,7 @@ from app.db.models import Pedido
 
 class OrderService:
         
-    def get_order(self, status, user, session):
-        
+    def get_order(self, status, session):
         if status:
             all_orders = session.query(Pedido).filter_by(status=status).all()
         else:
@@ -12,7 +11,6 @@ class OrderService:
         return all_orders
     
     def get_order_by_id(self, order_id, session):
-        
         order = session.query(Pedido).filter_by(id=order_id).first()
         if not order:
             raise HTTPException(status_code=404, detail="Order not found")
